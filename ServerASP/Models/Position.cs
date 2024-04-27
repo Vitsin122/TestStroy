6a53dbf04f924d019b532a61f8c7ebaba06332b1
Виталий Пахомович <warfire122@gmail.com>
2024-04-27 14:39:58 +0700

    Delete autoincrement attribute

--- ServerASP/Models/Position.cs
+++ ServerASP/Models/Position.cs
@@ -4,7 +4,6 @@ namespace ServerASP.Models
 {
     public class Position
     {
-        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public short Id { get; set; }
         public string PositionName { get; set; } = null!;
         public List<Employer>? Employers { get; set; } = new();
