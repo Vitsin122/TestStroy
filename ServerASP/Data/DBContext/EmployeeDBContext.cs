37bb0b1b83f0a60be3e143ed327bce40ed077d53
Виталий Пахомович <warfire122@gmail.com>
2024-05-07 13:03:02 +0700

    Revert "c"
    This reverts commit 0f9000232172e494ea6c7f89de0fe291b1f4790e.

---
+++ ServerASP/Data/DBContext/EmployeeDBContext.cs
@@ -0,0 +1,28 @@
+﻿using ServerASP.Models;
+using Microsoft.EntityFrameworkCore;
+using ServerASP.Data.ModelConfigure;
+
+namespace ServerASP.Data.DBContext
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<Employer> Employee { get; set; }
+        public DbSet<Position> Position {  get; set; }
+
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options)
+            : base(options)
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=EmployeeDB;Username=postgres;Password=vitalya122");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PositionConfig());
+            modelBuilder.ApplyConfiguration(new EmployerConfig());
+        }
+    }
+}
---
+++ ServerASP/MapperProfiles/EmployerPostProfile.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using ServerASP.Models;
+using ServerASP.Models.DTO.Incoming;
+
+namespace ServerASP.MapperProfiles
+{
+    public class EmployerPostProfile : Profile
+    {
+        public EmployerPostProfile()
+        {
+            CreateMap<EmployeeAddDTO, Employer>()
+                .ForMember(
+                    dest => dest.isActive,
+                    opt => opt.MapFrom(src => src.IsActive)
+                )
+                .ForMember(
+                    dest => dest.Firstname,
+                    opt => opt.MapFrom(src => src.FirstName)
+                )
+                .ForMember(
+                    dest => dest.Surname,
+                    opt => opt.MapFrom(src => src.SurName)
+                )
+                .ForMember(
+                    dest => dest.Lastname,
+                    opt => opt.MapFrom(src => src.LastName)
+                )
+                .ForMember(
+                    dest => dest.Id,
+                    opt => opt.MapFrom(src => 0)
+                )
+                .ForMember(
+                    dest => dest.Birthday,
+                    opt => opt.MapFrom(src => src.BirthDay)
+                )
+                .ForMember(
+                    dest => dest.Position,
+                    opt => opt.MapFrom(src => new Position { PositionName = src.Position })
+                )
+                .ForMember(
+                    dest => dest.Salary,
+                    opt => opt.MapFrom(src => src.Salary)
+                );
+        }
+    }
+}
